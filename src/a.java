351648b76ba850936fcf43deee678d7964d71e54
qiong1130 <1538877085@qq.com>
2014-12-18 19:33:05 +0800

    my solution to Lab2

---
+++ src/a.java
@@ -0,0 +1,14 @@
+class a{ 
+	public static void main(String[] a) 
+	{
+		int Sum;
+       		int i;
+       		i = 0;
+        	Sum = 0;
+        	while (i<101){
+        		Sum = Sum + i;
+        		i = i+1;
+        	}
+        	System.out.println(Sum);
+   	 }
+}
